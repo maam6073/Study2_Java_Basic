9938cba588122a5c501270c37d1333b5cd04df68
Dohyeong_Kim <maam10@naver.com>
2022-07-01 23:51:31 +0900

    0701java_기초공부

---
+++ First/src/classpart/Student.java
@@ -0,0 +1,18 @@
+package classpart;
+
+public class Student
+{
+	int studentId;			//학번
+	String studentName;		//학생 이름
+	int grade;				//학년
+	String address;			//사는 곳
+	
+	
+	public static void main(String[] args)
+	{
+		
+		
+
+	}
+
+}
