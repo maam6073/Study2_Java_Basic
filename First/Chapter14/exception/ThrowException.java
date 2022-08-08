82f3ac921e597a5fabebfc0abc075cb41f4df685
Dohyeong_Kim <maam10@naver.com>
2022-08-08 23:57:57 +0900

    14-3 throws progress

---
+++ First/Chapter14/exception/ThrowException.java
@@ -0,0 +1,25 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowException {
+	public Class loadClass(String fileName,String className) throws //두 예외를 메서드가 호출될 때 처리하도록 미룸
+	FileNotFoundException, ClassNotFoundException{
+		FileInputStream fis = new FileInputStream(fileName);
+		Class c = Class.forName(className);
+		return c;
+	}
+
+
+
+public static void main(String[] args)
+{
+	ThrowException test = new ThrowException();
+	try {
+		test.loadClass("a.txt","java.lang.String");		
+	}catch (FileNotFoundException | ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+}
+}
